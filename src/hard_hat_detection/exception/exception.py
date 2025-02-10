4c51f2d6263bcbaa121a3df30e42d1e01ed5c12f
Sri <30577467+SHK2805@users.noreply.github.com>
2025-02-10 14:22:25 +0000

    added exception logging to file

--- src/hard_hat_detection/exception/exception.py
+++ src/hard_hat_detection/exception/exception.py
@@ -12,9 +12,9 @@ class CustomException(Exception):
         self.file_name = exc_tb.tb_frame.f_code.co_filename
 
     def __str__(self):
-        return "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
+         message = "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
             self.file_name, self.lineno, str(self.error_message))
-
+        logger.error(message)
 
 if __name__ == '__main__':
     try:
